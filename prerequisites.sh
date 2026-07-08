2edfa154511adeb667b474df2dcebeae3d013496
Madheswaran Selvakumar <madheswaran.selvakumar@gmail.com>
2026-07-08 17:41:34 +0000

    helm integration files

--- prerequisites.sh
+++ prerequisites.sh
@@ -15,3 +15,6 @@ systemctl start docker
 git config --global user.name "Madheswaran Selvakumar"
 git config --global user.email "madheswaran.selvakumar@gmail.com"
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
